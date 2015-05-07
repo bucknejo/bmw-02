8a40c89b2594d47b40030bfcfe6e8c3f7de745c9
jbuckner <jon@redbonesolutions.com>
2015-05-07 14:02:18 -0400

    Add changes for attachment/document/itinerary

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -18,6 +18,7 @@ class AdminController extends Zend_Controller_Action {
         $ajaxContext->addActionContext('read', 'html');
         $ajaxContext->addActionContext('detail', 'html');
         $ajaxContext->addActionContext('list', 'html');
+        $ajaxContext->addActionContext('delete', 'json');        
         $ajaxContext->initContext();        
     }
     
@@ -220,6 +221,247 @@ class AdminController extends Zend_Controller_Action {
         $this->view->response = $participant_options;
         
     }
+    
+    public function uploadAction() {
+        
+        try {
+        
+            // HTTP headers for no cache etc
+            header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+            header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+            header("Cache-Control: no-store, no-cache, must-revalidate");
+            header("Cache-Control: post-check=0, pre-check=0", false);
+            header("Pragma: no-cache");
+            
+            $itinerary_type = $_REQUEST["itinerary_type"];
+            $document_name = $_REQUEST["document_name"];
+            
+            // Settings
+            //$targetDir = ini_get("upload_tmp_dir") . DIRECTORY_SEPARATOR . "plupload";        
+            $config = Zend_Registry::get('config');
+            //$targetDir = realpath($config->itinerary->upload->path) . DIRECTORY_SEPARATOR;
+            $targetDir = realpath($config->itinerary->upload->path);
+
+            $cleanupTargetDir = true; // Remove old files
+            //$maxFileAge = 5 * 3600; // Temp file age in seconds
+            $maxFileAge = 60 * 60 * 24 * 365; // Temp file age in seconds
+
+            // 5 minutes execution time
+            @set_time_limit(5 * 60);
+            
+            // Get parameters
+            $chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
+            $chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
+            $fileName = isset($_REQUEST["name"]) ? $_REQUEST["name"] : '';
+
+            // Clean the fileName for security reasons
+            $fileName = preg_replace('/[^\w\._]+/', '_', $fileName);
+            
+            // Make sure the fileName is unique but only if chunking is disabled
+            if ($chunks < 2 && file_exists($targetDir . DIRECTORY_SEPARATOR . $fileName)) {
+                $ext = strrpos($fileName, '.');
+                $fileName_a = substr($fileName, 0, $ext);
+                $fileName_b = substr($fileName, $ext);
+
+                $count = 1;
+                while (file_exists($targetDir . DIRECTORY_SEPARATOR . $fileName_a . '_' . $count . $fileName_b)) {
+                    $count++;                    
+                }
+
+                $fileName = $fileName_a . '_' . $count . $fileName_b;
+            }
+            
+            $filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+
+            // Create target dir
+            if (!file_exists($targetDir)) {
+                @mkdir($targetDir);
+            }
+                
+            // Remove old temp files	
+            if ($cleanupTargetDir && is_dir($targetDir) && ($dir = opendir($targetDir))) {
+                while (($file = readdir($dir)) !== false) {
+                    $tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
+                    
+                    $max = time() - $maxFileAge;
+                    $filetime = filemtime($tmpfilePath);
+                    $match = preg_match('/\.part$/', $file);
+
+                    // Remove temp file if it is older than the max age and is not the current file
+                    //if (preg_match('/\.part$/', $file) && (filemtime($tmpfilePath) < time() - $maxFileAge) && ($tmpfilePath != "{$filePath}.part")) {
+                    if (($filetime < $max) && ($tmpfilePath != "{$filePath}.part")) {
+                        @unlink($tmpfilePath);
+                    }
+                }
+
+                closedir($dir);
+            } else {
+                $response = '{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}';
+                die($response);           
+            }
+            
+            // Look for the content type header
+            if (isset($_SERVER["HTTP_CONTENT_TYPE"]))
+                    $contentType = $_SERVER["HTTP_CONTENT_TYPE"];
+
+            if (isset($_SERVER["CONTENT_TYPE"]))
+                    $contentType = $_SERVER["CONTENT_TYPE"];
+
+            // Handle non multipart uploads older WebKit versions didn't support multipart in HTML5
+            if (strpos($contentType, "multipart") !== false) {
+                if (isset($_FILES['file']['tmp_name']) && is_uploaded_file($_FILES['file']['tmp_name'])) {
+                    // Open temp file
+                    $out = fopen("{$filePath}.part", $chunk == 0 ? "wb" : "ab");
+                    if ($out) {
+                        // Read binary input stream and append it to temp file
+                        $in = fopen($_FILES['file']['tmp_name'], "rb");
+                                                
+                        if ($in) {
+                            while ($buff = fread($in, 4096)) {
+                                fwrite($out, $buff);                            
+                            }
+                        } else {
+                            $response = '{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}';
+                            die($response);                        
+                        }
+                        fclose($in);
+                        fclose($out);
+                        @unlink($_FILES['file']['tmp_name']);
+                    } else {
+                        $response = '{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}';
+                        die($response);
+                    }
+                } else {
+                    $response = '{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}';
+                    die($response);               
+                }
+            } else {
+                // Open temp file
+                $out = fopen("{$filePath}.part", $chunk == 0 ? "wb" : "ab");
+                if ($out) {
+                    // Read binary input stream and append it to temp file
+                    $in = fopen("php://input", "rb");
+                                        
+                    if ($in) {
+                        while ($buff = fread($in, 4096)) {
+                            fwrite($out, $buff);                        
+                        }
+                    } else {
+                        $response = '{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}';
+                        die($response);                    
+                    }
+
+                    fclose($in);
+                    fclose($out);
+                } else {
+                    die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');                
+                }
+            }
+            
+            // Check if file has been uploaded
+            if (!$chunks || $chunk == $chunks - 1) {
+                // Strip the temp .part suffix off 
+                rename("{$filePath}.part", $filePath);  
+                
+                $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                $mime = finfo_file($finfo, $filePath);
+                finfo_close($finfo);
+
+                $mapper = new Application_Model_TableMapper();   
+                $table_name = "documents";
+                $data = array(
+                    'date_created' => date('Y-m-d'),
+                    'type' => $itinerary_type,
+                    'name' => $fileName,
+                    //'itinerary' => file_get_contents($filePath),
+                    'file_name' => $document_name,
+                    'mime' => $mime
+                );
+
+                $document = $mapper->insertItem($table_name, $data);
+                $id = -1;
+                if ($document > 0) {
+                    $id = $mapper->getLastInsertId($table_name);
+                }
+                
+            }                
+                        
+            // Return JSON-RPC response
+            $response = '{"jsonrpc" : "2.0", "result" : null, "id" : "id", "document": ' . $id . '}';                                    
+            
+            $this->view->response = $response;
+            die($response);            
+            
+        } catch (Exception $ex) {
+
+        }
+        
+    }
+    
+    
+    public function deleteAction() {
+        
+        $document = $this->_getParam("document", "-1");
+        
+        $mapper = new Application_Model_TableMapper();   
+        $table_name = "documents";
+        $id = $mapper->deleteItem($table_name, $document);
+        
+        $code = -1;
+        $message = "fail";
+        if ($id > 0) {
+            $code = 0;
+            $message = "success";
+        }
+                
+        $data = array(
+            'code' => $code,
+            'message' => $message,
+            'id' => $id,
+            'document' => $document
+        );
+        $response = json_encode($data);
+        $this->view->response = $response;
+    }
+    
+    function downloadAction() {
+        
+        $id = $this->_getParam("document", "-1");
+        
+        $mapper = new Application_Model_TableMapper();
+        $table_name = "documents";
+        $documents = $mapper->getItemById($table_name, $id);
+        
+        $config = Zend_Registry::get('config');
+        //$targetDir = realpath($config->itinerary->upload->path) . DIRECTORY_SEPARATOR;
+        $targetDir = realpath($config->itinerary->upload->path);
+        
+                
+        if (count($documents) > 0) {
+            
+            $document = $documents[0];
+            
+            $filePath = $targetDir . DIRECTORY_SEPARATOR . $document["name"];
+            $mime = $document["mime"];
+            $filename = $document["file_name"];
+            header("Content-type: $mime");
+            header("Content-Disposition: attachment; filename=\"".$filename."\"");            
+            //header("Content-length: $fsize");
+            //header("Cache-control: private");  
+            set_time_limit(0);
+            $file = @fopen($filePath,"rb");
+            while(!feof($file))
+            {
+                print(@fread($file, 1024*8));
+                ob_flush();
+                flush();
+            }
+        
+        }
+        
+        
+    }
+    
     
         
 }
